cdc2ebb071f1119956022997c50cf43567d0cb9f
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-04-04 10:07:21 +0200

    adjusted subscription-service settings

--- subscription-service/src/main/java/com/example/subscriptionservice/SubscriptionServiceApplication.java
+++ subscription-service/src/main/java/com/example/subscriptionservice/SubscriptionServiceApplication.java
@@ -1,13 +1,20 @@
 package com.example.subscriptionservice;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example")
+@EnableDiscoveryClient
+@EnableMongoRepositories(basePackages = "com.example.subscriptionservice")
 public class SubscriptionServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SubscriptionServiceApplication.class, args);
+    public static void main(String... args) {
+        new SpringApplicationBuilder(SubscriptionServiceApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
     }
 
 }
